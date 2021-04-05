f90ab593dfdf6e4679506835bedf25610eefc1ab
gombbang <evaasuka2015@naver.com>
2021-04-05 18:38:21 +0900

    4th finish server_dev. Save code before starting to make 'to-do list' app.

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import RegistUser, AppLogin
+from . import views
 
 # ~/login/regist_user
 urlpatterns = [
-    url('regist_user', RegistUser.as_view(), name='regist_user'),
-    url('app_login', AppLogin.as_view(), name='app_login'),
+    url('regist_user', views.RegistUser.as_view(), name='regist_user'),
+    url('app_login', views.AppLogin.as_view(), name='app_login'),
 ]
--- login/views.py
+++ login/views.py
@@ -2,41 +2,38 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LoginUser
 from django.contrib.auth.hashers import make_password, check_password
+from .serialize import LoginUserSerializer
 
 
 class AppLogin(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-
+        user_id = request.data.get('user_id', "")
+        user_pw = request.data.get('user_pw', "")
         user = LoginUser.objects.filter(user_id=user_id).first()
 
         if user is None:
-            return Response(dict(msg="해당 사용자가 없습니다."))
-
+            return Response(dict(msg="해당 ID의 사용자가 없습니다."))
         if check_password(user_pw, user.user_pw):
-            return Response(dict(msg="로그인 성공"))
+            return Response(dict(msg="로그인 성공", user_id=user.user_id, birth_day=user.birth_day,
+                                 gender=user.gender, email=user.email, name=user.name, age=user.age))
         else:
-            return Response(dict(msg="로그인 실패. 비밀번호 틀림!"))
+            return Response(dict(msg="로그인 실패. 패스워드 불일치"))
 
 
 class RegistUser(APIView):
     def post(self, request):
-        user_id = request.data.get('user_id')
-        user_pw = request.data.get('user_pw')
-        user_pw_encrypted = make_password(user_pw)
+        serializer = LoginUserSerializer(request.data)
 
-        # if user_id가 특수문자인지, 숫자인지, 한글인지
+        if LoginUser.objects.filter(user_id=serializer.data['user_id']).exists():
+            # DB에 있는 값 출력할 때 어떻게 나오는지 보려고 user 객체에 담음
+            user = LoginUser.objects.filter(user_id=serializer.data['user_id']).first()
+            data = dict(
+                msg="이미 존재하는 아이디입니다.",
+                user_id=user.user_id,
+                user_pw=user.user_pw
+            )
+            return Response(data)
 
-        user = LoginUser.objects.filter(user_id=user_id).first()
-        if user is not None:
-            return Response(dict(msg="동일안 아이디가 있습니다."))
+        user = serializer.create(request.data)
 
-        LoginUser.objects.create(user_id=user_id, user_pw=user_pw_encrypted)
-
-        data = dict(
-            user_id=user_id,
-            user_pw=user_pw_encrypted
-        )
-
-        return Response(data)
+        return Response(data=LoginUserSerializer(user).data)
